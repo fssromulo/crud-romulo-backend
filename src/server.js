a4e2442c762e9a70a4f64e17ed20d6c24681db75
romulofss <romulo.fernando@paytrack.com.br>
2021-03-04 01:59:55 -0300

    Ajuste nome variavel de ambiente e porta 'reserva'

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const routes = require("./routes");
 const server = require("http").Server(app);
-const port = process.env.PORT || 3333;
+const port = process.env.SERVER_PORT || 3001;
 
 app.use(express.json());
 app.use(cors());
