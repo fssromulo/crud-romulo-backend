4dd69003fc2d64d8a4d853dc128223ec5b6e7523
romulofss <romulo.fernando@paytrack.com.br>
2021-03-08 10:05:45 -0300

    Ajuste deploy heroku.

--- src/server.js
+++ src/server.js
@@ -3,13 +3,13 @@ const express = require("express");
 const app = express();
 const routes = require("./routes");
 const server = require("http").Server(app);
-const port = process.env.SERVER_PORT || 3001;
+const HOST = process.env.SERVER_SERVER || "0.0.0.0";
+const PORT = process.env.SERVER_PORT || 3001;
 
 app.use(express.json());
 
 app.use(cors());
-
 app.use("/", routes);
-server.listen(port, () => {
-	console.log(`Running on port ${port}`);
+server.listen(PORT, HOST, () => {
+	console.log(`Running on port ${PORT}`);
 });
